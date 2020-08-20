a0dd38264b179bf8b6b623a5dc29c64834a05032
robibrutoczki <robibrutoczki@googlemail.com>
2020-08-20 16:03:41 +0000

    Added contact page,email.js

--- Gym/settings.py
+++ Gym/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'about_us',
+    'contact',
 
 
     # Other
--- Gym/urls.py
+++ Gym/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('about_us/', include('about_us.urls')),
+    path('contact/', include('contact.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
