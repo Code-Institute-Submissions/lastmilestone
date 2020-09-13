c0301f92cb5738d404af6d8f2e7a62db3de23f40
robibrutoczki <robibrutoczki@googlemail.com>
2020-09-13 13:59:47 +0000

    Footer links added.Added Views,urls to about page. Full CRUD functions to modify  trainers.

--- about_us/urls.py
+++ about_us/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_us, name='about_us'),
-
+    
+    path('', views.about_us, name='about_us') ,
+    path('<int:trainer_id>/', views.trainer_detail, name='trainer_detail'),
+    path('add/', views.add_trainer, name='add_trainer'),
+    path('edit/<int:trainer_id>/', views.edit_trainer, name='edit_trainer'),
+    path('delete/<int:trainer_id>/',
+         views.delete_trainer, name='delete_trainer'),
 ]
--- about_us/views.py
+++ about_us/views.py
@@ -1,7 +1,103 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Trainers
+from .forms import TrainerForm
 # Create your views here.
 
 
+
+
 def about_us(request):
-    return render(request, 'about_us/about_us.html')
+    """ A view to show individual trainer details """
+    trainer = Trainers.objects.all()
+    
+
+    context = {
+        'trainer': trainer,
+    }
+
+    return render(request,'about_us/about_us.html', context)
+
+def trainer_detail(request, trainer_id):
+    """ A view to show individual trainer details """
+
+    trainer = get_object_or_404(Trainers, pk=trainer_id)
+
+    context = {
+        'trainer': trainer,
+    }
+
+    return render(request, 'about_us/trainer_details.html', context)
+
+
+@login_required
+def add_trainer(request):
+    """ Add a trainer to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES)
+        if form.is_valid():
+            trainer = form.save()
+            messages.success(request, 'Successfully added trainer!')
+            return redirect(reverse('trainer_detail', args=[trainer.id]))
+        else:
+            messages.error(
+                request, 'Failed to add trainer. \
+                    Please ensure the form is valid.')
+    else:
+        form = TrainerForm()
+
+    template = 'about_us/add_trainer.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_trainer(request, trainer_id):
+    """ Edit a trainer in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    trainer = get_object_or_404(Trainers, pk=trainer_id)
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES, instance=trainer)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated trainer!')
+            return redirect(reverse('trainer_detail', args=[trainer.id]))
+        else:
+            messages.error(
+                request, 'Failed to update trainer.\
+                     Please ensure the form is valid.')
+    else:
+        form = TrainerForm(instance=trainer)
+        messages.info(request, f'You are editing {trainer.name}')
+
+    template = 'about_us/edit_trainer.html'
+    context = {
+        'form': form,
+        'trainer': trainer,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_trainer(request, trainer_id):
+    """ Delete a trainer from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    trainer = get_object_or_404(Trainers, pk=trainer_id)
+    trainer.delete()
+    messages.success(request, 'Trainer deleted!')
+    return redirect(reverse('about_us'))
