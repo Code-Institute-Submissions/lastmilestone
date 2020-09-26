85bef42b6a6727a9273554a1a4546a992cb57766
robibrutoczki <robibrutoczki@googlemail.com>
2020-09-26 15:49:49 +0000

    Cleaned some code

--- about_us/models.py
+++ about_us/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Trainers(models.Model):
-   
+
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
--- about_us/urls.py
+++ about_us/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.about_us, name='about_us') ,
+
+    path('', views.about_us, name='about_us'),
     path('<int:trainer_id>/', views.trainer_detail, name='trainer_detail'),
     path('add/', views.add_trainer, name='add_trainer'),
     path('edit/<int:trainer_id>/', views.edit_trainer, name='edit_trainer'),
