68eb0c16392cc30f12496fdd22ba96ff45477015
robibrutoczki <robibrutoczki@googlemail.com>
2020-10-01 19:29:11 +0000

    Changes in trainers CSS and Table

--- about_us/models.py
+++ about_us/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Trainers(models.Model):
 
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
